0a483a49bf479798a8eee5a0436e29d940a19124
Nelo28 <chinelochukwuka.cc@gmail.com>
2022-09-08 14:21:03 -0700

    8. UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,9 @@
  * main - Print the string in the write function
  *
  * Description: using the main function
- * this program prints "and that piece of art is useful - Dora Korpar, 2015-10-19", followed by a new line, to standard error.
- * Return: 1
+ * this program prints "and that piece of art is useful - Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
+ * Return: Always 1 (Success) 
  */
 int main(void)
 {
