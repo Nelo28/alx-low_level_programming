7bbacd46842255d4ae5c901386d26e68eceae5ed
Nelo28 <chinelochukwuka.cc@gmail.com>
2022-09-12 03:41:07 -0700

    Task 6 Numberz

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry Point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar(d);
-	putchar(p);
-	if (d == '8' && p == '9')
-		continue;
+	int c = 0
+	int f_d
+	int l_d
 
-	putchar(',');
-	putchar(' ');
+	while (c <= 99)
+	{
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+
+		if f_d < l_d
+		{
+			putchar(f_d);
+			putchar(l_d);
+
+			if (c != 89)
+			{
+				putchar (',');
+				putchar (' ');
+			}
+		}
+		c++;
+	}
+	putchar ('\n')
+	return (0);
 }
