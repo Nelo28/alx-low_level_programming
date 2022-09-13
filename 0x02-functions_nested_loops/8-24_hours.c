d16230e1f345fdd9a0e57a68656e8dd9cfcb0ef1
Nelo28 <chinelochukwuka.cc@gmail.com>
2022-09-13 15:52:39 -0700

    Task 8 federal agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,12 +8,11 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 	int hours_remainder;
-
 	while (hours <= 23)
 	{
 	while (minutes <= 59)
 	{
-av
+	mins_remainder = minutes % 10
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0')
