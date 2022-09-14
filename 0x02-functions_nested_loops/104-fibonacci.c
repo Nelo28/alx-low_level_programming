483a98af1208fdfb991c350deeea4c47e5ba17c5
Nelo28 <chinelochukwuka.cc@gmail.com>
2022-09-14 11:32:41 -0700

    Task 16 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 int inc;
-unsigned long n1 = 1, n2 = 2, n3;
+unsigned long n1 = 0, n2 = 1, n3;
 for (inc = 0; inc < 98; inc++)
 {
 n3 = n1 + n2;
 printf("%lu", n3);
 n1 = n2;
 n2 = n3;
-if (inc == 97)
+if (inc == 98)
 printf("\n");
 else
 printf(", ");
