dc768f44d0a223dfe67ac98b0777d97512761936
Nelo28 <chinelochukwuka.cc@gmail.com>
2022-09-19 09:19:53 -0700

    Task 7 Winning is only half of it

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,13 @@
 void puts_half(char *str)
 {
 	int len = 0, i, n = (len - 1) / 2;
+
 	while (str[len] != '\0')
 		len++;
 	if (len % 2 == 0)
 		n = (len - 1) / 2;
 	else
-		n = (len + 1) / 2;
+		n = (len - 1) / 2;
 	for (i = n; i < len; i++)
 		putchar(str[i]);
 	putchar('\n');
