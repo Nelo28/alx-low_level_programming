98272cacd6520daa88ad16d652ae9dd33cce2fc9
Nelo28 <chinelochukwuka.cc@gmail.com>
2022-10-25 10:13:36 -0700

    Task 13 Free list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _ra - reallocates memory for an array of pointers
@@ -29,6 +31,7 @@ listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 /**
  * free_listint_safe - frees a listint_t linked list.
  * @head: double pointer to the start of the list
+ *
  * Return: the number of nodes in the list
  */
 size_t free_listint_safe(listint_t **head)
