1a4ac5d467add7d08c7b1106dfc7a9ca19bd505a
Nelo28 <chinelochukwuka.cc@gmail.com>
2022-10-25 12:03:43 -0700

    Task 13 Free safe version

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -31,7 +31,6 @@ listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 /**
  * free_listint_safe - frees a listint_t linked list.
  * @head: double pointer to the start of the list
- *
  * Return: the number of nodes in the list
  */
 size_t free_listint_safe(listint_t **head)
